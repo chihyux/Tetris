dc7d0075d16c92cc4c16f2cc38ca37566ff930f7
chihyux <chihyux@gmail.com>
2020-08-11 01:02:34 +0800

    complete stage and style

---
+++ src/components/styles/StyledDisplay.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+
+export const StyledDisplay = styled.div`
+    box-sizing: border-box;
+    display: flex;
+    align-items: center;
+    margin: 0 0 20px 0;
+    padding: 1rem;
+    border: 3px solid #999;
+    min-height: 30px;
+    width: 100%;
+    border-radius: 10px;
+    color: ${props => props.gameover? 'red': '#999'};
+    font-family: Pixel, Arial;
+    font-size: 0.8rem;
+`
---
+++ src/components/styles/StyledIndex.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components'
+
+export const StyledIndexWrapper = styled.div`
+    width: 100vw;
+    max-width: 100%;
+`
